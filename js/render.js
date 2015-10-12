a6a9d86989201767283edbdc4fbb56b21a77901c
Johan Boqvist <johanboqvist@Johans-MacBook-Pro.local>
2015-10-12 17:46:54 +0200

    massa idioti

--- js/render.js
+++ js/render.js
@@ -3,9 +3,9 @@ var GAME = GAME || {};
 GAME.Renderer = (function () {
     'use strict';
     
-    var canvas = document.getElementById("gameCanvas"),
+    var canvas = document.getElementById('gameCanvas'),
         context = canvas.getContext('2d'),
-        fpsNode = document.getElementById("fps"),
+        fpsNode = document.getElementById('fps'),
         
         bufferCanvas = document.createElement('canvas'),
         bufferContext = bufferCanvas.getContext('2d'),
@@ -22,37 +22,28 @@ GAME.Renderer = (function () {
             
 
             var cropX = 0,
-                cropY = 17*3 + 1;
-      
-              
-          // bufferContext.translate(translateX, translateY);
+                cropY = 17*8 + 1;
             
             
             for (y = top; y < top + 15; y += 1) {
 
                 for (x = left; x < left + 17; x += 1) {
 
-                    //if(x < 0 || y < 0) continue;
-
                     switch (level1[x + y * 32]) {
 
                         case 0:
-                            //bufferContext.fillStyle = "#009900";
                             cropX = 1;
                            
                             break;
                         case 1:
-                            //bufferContext.fillStyle = "#CCC";
                             cropX = 17*1 + 1;
                          
                             break;
                         case 2:
-                            //bufferContext.fillStyle = "#888";
                             cropX = 17*2 + 1;
                            
                             break;
                         case 3:
-                            //bufferContext.fillStyle = "#444";
                             cropX = 17*1 + 1;
                             break;
 
@@ -69,7 +60,7 @@ GAME.Renderer = (function () {
             }
             
 
-            context.drawImage(bufferCanvas, 0, 0);
+            context.drawImage(bufferCanvas, 0,0);
          
            
 
@@ -119,13 +110,11 @@ GAME.Renderer = (function () {
 
         
         
-    
-  
-   
-    canvas.style.width = 768+"px";
-    canvas.style.height = 672 + "px";
     canvas.width = GAME.screenSize.width;
     canvas.height = GAME.screenSize.height;
+    canvas.style.width  = '512px';
+    canvas.style.height = '448px';
+   
     
     context.imageSmoothingEnabled = false;
     window.devicePixelRatio = 1;
@@ -134,18 +123,13 @@ GAME.Renderer = (function () {
     bufferCanvas.height = canvas.height;
     
     
-    context.lineWidth = 1+"px";
-    context.strokeWidth = 1+"px";
-    bufferContext.lineWidth = 1+"px";
-    bufferContext.strokeWidth = 1+"px";
+    context.lineWidth = 1;
+    context.strokeWidth = 1;
+    bufferContext.lineWidth = 1;
+    bufferContext.strokeWidth = 1;
     bufferContext.imageSmoothingEnabled = false;
     
 
-    
-    
-  
-    
-    
     return { render : render,
              updateFPS: updateFPS};
     
